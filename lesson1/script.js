c64dc64787b978c4e6bdc570cf41457801895ae7
Kostea Shapk <taec85@mail.ru>
2020-02-12 20:04:27 +0200

    third commit

--- lesson1/script.js
+++ lesson1/script.js
@@ -1,5 +1,25 @@
-'use strict';
+"use strict";
 
-let answer = prompt ("Есть ли вам 18", "");
+let money = +prompt("Ваш бюджет на месяц?", "");
 
-console.log(typeof(answer));
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+let state = prompt("Введите обязательную статью расходов в этом месяце", "сигареты");
+
+let sigMoney = +prompt("Во сколько обойдется?", "");
+
+let appData = {
+
+    budget: money,
+    timeData: time,
+    expenses: {
+        state: sigMoney
+    },
+    optionalExpenses: "",
+    income: "",
+    savings: false
+};
+
+
+
+alert(sigMoney / 30);
